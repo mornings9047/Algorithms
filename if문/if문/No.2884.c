48530397eb9b26c4e4a24c50874bdf5f51a4b45e
JoonHee <mornings9047@naver.com>
2020-04-10 14:25:29 +0900

    2단계 완료

--- if문/if문/No.2884.c
+++ if문/if문/No.2884.c
@@ -2,5 +2,20 @@
 #include <stdio.h>
 
 int main() {
+	int hour, min;
+	scanf("%d %d", &hour, &min);
 
+	if (min >= 45)
+		min -= 45;
+	else {
+		if (hour == 0) {
+			hour = 23;
+			min += 15;
+		}
+		else {
+			hour--;
+			min += 15;
+		}
+	}
+	printf("%d %d \n", hour, min);
 }
