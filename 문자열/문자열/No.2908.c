5c15bcdeed2bc8adacdac0aee44a622eb99909b0
JoonHee <mornings9047@naver.com>
2020-04-15 15:47:02 +0900

    No.2908 완료

--- 문자열/문자열/No.2908.c
+++ 문자열/문자열/No.2908.c
@@ -1,16 +1,35 @@
 /* No.2908 숫자를 뒤집어서 비교하는 문제 */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int reverse(char *);
 
 int main() {
-	char str1[4], str2[4];
+	char str1[10], str2[10];
 	int num1, num2, max;
 	scanf("%s %s", str1, str2);
 
-	num1 = (str1[2] - 48) * 100 + (str1[1] - 48) * 10 + str1[0] - 48;
-	num2 = (str2[2] - 48) * 100 + (str2[1] - 48) * 10 + str2[0] - 48;
+	num1 = reverse(str1);
+	num2 = reverse(str2);
 
 	max = (num1 > num2 ? num1 : num2);
 	printf("%d \n", max);
 
 	return 0;
+}
+
+int reverse(char * str) {
+	int num;	// 뒤집은 숫자
+	int length = strlen(str);	// 문자열의 길이
+
+	for (int i = 0; i < length / 2; i++) {
+		int temp = str[i];
+		str[i] = str[length - 1 - i];
+		str[length - 1 - i] = temp;
+	}
+
+	num = atoi(str);
+
+	return num;
 }
