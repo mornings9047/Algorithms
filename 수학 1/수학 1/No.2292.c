6e2a884219473992120f6281367ebfa8b2ebab96
JoonHee <mornings9047@naver.com>
2020-04-17 18:45:16 +0900

    No.2292 완료

---
+++ 수학 1/수학 1/No.2292.c
@@ -0,0 +1,19 @@
+/* No.2292 벌집이 형성되는 규칙에 따라 벌집의 위치를 구하는 문제 */
+#include <stdio.h>
+
+int main() {
+	int num, cnt = 1;
+	int max = 1, outer = 0;		// outer: 최외각에 있는 제일 큰 숫자 + 1 (0, 6, 18, 36, 60 ...) + 1
+	int d = 6;		// 육각형이므로 6을 이용
+
+	scanf("%d", &num);
+	while (1) {
+		if (num <= outer + 1)
+			break;
+
+		outer += d * cnt++;
+	}
+
+	printf("%d \n", cnt);
+	return 0;
+}
