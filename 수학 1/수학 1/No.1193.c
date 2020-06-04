00adc7a62d9f2f5f6163bb70c872d6a8d918d17b
JoonHee <mornings9047@naver.com>
2020-06-04 16:28:21 +0900

    No.1193 완료

--- 수학 1/수학 1/No.1193.c
+++ 수학 1/수학 1/No.1193.c
@@ -3,10 +3,29 @@
 
 int main() {
 	int input;
+	int sum = 0, inc = 1;	// 대각선의 분모, 분자의 합이 1+1, 1+2, 1+3 ... 으로 증가, 배열이 inc × inc 로 결정됨
+	int up, down;	// 분자, 분모
+
 	scanf("%d", &input);
+	while (sum + inc < input)
+		sum += inc++;
 
-
-
-
+	if (inc % 2 == 0) {
+		up = inc;
+		down = 1;
+		for (int i = 0; i < sum + inc - input; i++) {
+			up--;
+			down++;
+		}
+	}
+	else {
+		up = 1;
+		down = inc;
+		for (int i = 0; i < sum + inc - input; i++) {
+			up++;
+			down--;
+		}
+	}
+	printf("%d/%d \n", up, down);
 	return 0;
 }
