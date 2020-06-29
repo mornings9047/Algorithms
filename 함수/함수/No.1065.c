381a8aba95e8bd1518e1e990a9b3ed28da0d6be8
JoonHee <mornings9047@naver.com>
2020-06-29 17:31:01 +0900

    7단계 완료

--- 함수/함수/No.1065.c
+++ 함수/함수/No.1065.c
@@ -1,23 +1,21 @@
 /* No.1065 X가 한수인지 판별하는 함수를 정의하여 문제를 해결해 봅시다. */
 #include <stdio.h>
 
-int d(int);
-int arr[3];
+int han(int);
 
 int main() {
 	int num;
 	scanf("%d", &num);
-	printf("%d \n", d(num));
+	printf("%d \n", han(num));
 	return 0;
 }
 
-int d(int num) {
-	int a, b, c, cnt = 0;
-	if (num / 10 == 0 || num / 100 == 0)
+int han(int num) {
+	int a, b, c, cnt = 99;
+	if (num < 100)
 		return num;
 
-	cnt = 99;
-	for (int i = 100; i <= num; i++) {
+	for (int i = 101; i <= num; i++) {
 		a = i / 100;
 		b = i % 100 / 10;
 		c = i % 10;
@@ -25,6 +23,5 @@ int d(int num) {
 		if (a - b == b - c)
 			cnt++;
 	}
-
 	return cnt;
 }
