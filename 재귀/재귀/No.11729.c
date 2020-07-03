a97b8644d5ae4a6296d5721cd08b5b8b2871d456
JoonHee <mornings9047@naver.com>
2020-07-03 13:37:20 +0900

    11단계 완료

--- 재귀/재귀/No.11729.c
+++ 재귀/재귀/No.11729.c
@@ -1,15 +1,20 @@
 /* No.11729 재귀적인 패턴을 재귀함수로 찍는 문제 2 */
 #include <stdio.h>
-
-void hanoi(int, int, int);
+#include <math.h>
+void hanoi(int, int, int, int);
 
 int main() {
 	int num;
 	scanf("%d", &num);
-	hanoi(1, 3, num);
+	printf("%d \n", (1 << num) - 1);
+	hanoi(1, 2, 3, num);
 	return 0;
 }
 
-void hanoi(int start, int temp, int num) {
-	
+void hanoi(int start, int temp, int end, int num) {
+	if (num == 0)
+		return;
+	hanoi(start, end, temp, num - 1);
+	printf("%d %d \n", start, end);
+	hanoi(temp, start, end, num - 1);
 }
