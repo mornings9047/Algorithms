e7955b7a44e72a831ff6a34b2d754deca5cf2077
JoonHee <mornings9047@naver.com>
2020-07-14 20:19:19 +0900

    20단계 완료

--- 스택/스택/No.1874.c
+++ 스택/스택/No.1874.c
@@ -4,37 +4,55 @@
 #define MAX 100000
 
 int stack[MAX];
+char result[MAX * 2];
 int size = 0;
-
-int isEmpty() {
-	return size == 0;
-}
-
-int isFull() {
-	return size == MAX + 1;
-}
+int rsize = 0;
 
 void push(int num) {
-	printf("+ \n");
-	if (!isFull())
-		stack[size++] = num;
+	stack[size++] = num;
 }
 
 int pop() {
-	return isEmpty() ? 0 : stack[--size];
+	return stack[--size];
 }
 
-void printResult(int * arr, int cnt) {
+void push_r(char ch) {
+	result[rsize++] = ch;
+}
 
+void printResult() {
+	for (int i = 0; i < rsize; i++)
+		printf("%c \n", result[i]);
 }
 
 int main() {
-	int cnt;
+	int cnt, max = 0, result = 1;
 	int * arr;
 	scanf("%d", &cnt);
 	arr = (int *)malloc(sizeof(int) * cnt);
-	for (int i = 0; i < cnt; i++)
+	for (int i = 0; i < cnt; i++) {
 		scanf("%d", &arr[i]);
-	printResult(arr, cnt);
+		if (!result)
+			continue;
+		if (max < arr[i]) {
+			for (int j = max + 1; j <= arr[i]; j++) {
+				push(j);
+				push_r('+');
+			}
+			pop();
+			push_r('-');
+			max = max < arr[i] ? arr[i] : max;
+		}
+		else {
+			if (arr[i] == pop())
+				push_r('-');
+			else
+				result = 0;
+		}
+	}
+	if(result)
+		printResult();
+	else
+		printf("NO \n");
 	return 0;
 }
