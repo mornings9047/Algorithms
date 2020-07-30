b7ebe8efc7f4978e67b9322c68ffb2709a9aaf89
JoonHee <mornings9047@naver.com>
2020-07-30 10:00:56 +0900

    No.15649 구현중

---
+++ 백트래킹/백트래킹/No.15649.c
@@ -0,0 +1,13 @@
+/* No.15649 백트래킹 입문 문제 1 */
+#include <stdio.h>
+
+void backTraking(int num1, int num2) {
+
+}
+
+int main() {
+	int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	backTraking(num1, num2);
+	return 0;
+}
